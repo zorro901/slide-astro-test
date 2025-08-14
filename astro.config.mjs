69c0a1bc9ea870d94ac5b26cc2db8b569b55db19
zoror901 <zoro907@gmail.com>
2025-08-14 09:12:36 +0900

    setting github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jsulpis.github.io",
-	base: "/slides-template",
+	site: "https://zorro901.github.io",
+	base: "/slide-astro-test",
 });
