4aa9eeee66082d53478a95ba8a2e87720a26a661
zoror901 <zoro907@gmail.com>
2025-08-14 09:21:59 +0900

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://zorro901.github.io",
-	base: "/slide-astro-test",
+	base: "/slides",
 });
