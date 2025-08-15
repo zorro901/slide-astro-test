af131c02c4eb433c325b84ab747d88ee1be4326e
zoror901 <zoro907@gmail.com>
2025-08-15 09:54:34 +0900

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://zorro901.github.io",
-	base: "/slides",
+  site: "https://zorro901.github.io",
+  base: "/slides",
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
